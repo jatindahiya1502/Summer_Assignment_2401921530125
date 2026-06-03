9e0287b6260c64e0ca9e3f8385a03c31e68246a3
Jatin Dahiya <jatincode15@gmail.com>
2026-06-03 19:23:35 +0530

    Create SquaresofaSortedArray.cpp

---
+++ Week_1/Day_3/SquaresofaSortedArray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> ans(nums.size());
+
+        int i = 0 , j = nums.size()-1,k=nums.size()-1;
+        while (i<=j){
+
+            if(abs(nums[i])>abs(nums[j])){
+                ans[k]= nums[i]*nums[i];  
+                i++;
+            }
+            else{
+                ans[k]= nums[j]*nums[j];
+                j--;
+            }
+            k--;
+        }
+        return ans;
+    }
+};
