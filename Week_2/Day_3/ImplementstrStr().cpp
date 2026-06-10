24e7d4b1de84e3ecaf576568f585b3521c6bba73
Jatin Dahiya <jatincode15@gmail.com>
2026-06-10 22:26:31 +0530

    Create ImplementstrStr().cpp

---
+++ Week_2/Day_3/ImplementstrStr().cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.size()>haystack.size()){
+            return -1;
+        }
+        for(int i = 0 ; i <=haystack.size()-needle.size();i++){
+            for(int j = i; j<(i+needle.size());j++){
+                if(haystack[j]!=needle[j-i]){
+                    break;
+                }
+                else if(j==(i+needle.size()-1)){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
