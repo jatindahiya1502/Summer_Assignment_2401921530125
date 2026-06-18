8f2fd4541a9750f3dabee3ca9cdd0c3954153c14
Jatin Dahiya <jatincode15@gmail.com>
2026-06-18 23:19:36 +0530

    Create EvaluateReversePolishNotation.cpp

---
+++ Week_3/Day_4/EvaluateReversePolishNotation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+
+        stack<int> st;
+
+        for(auto token : tokens) {
+
+            if(token == "+" || token == "-" ||
+               token == "*" || token == "/") {
+
+                int num1 = st.top();
+                st.pop();
+
+                int num2 = st.top();
+                st.pop();
+
+                if(token == "+") st.push(num2 + num1);
+                else if(token == "-") st.push(num2 - num1);
+                else if(token == "*") st.push(num2 * num1);
+                else st.push(num2 / num1);
+            }
+            else {
+                st.push(stoi(token));
+            }
+        }
+
+        return st.top();
+    }
+};
